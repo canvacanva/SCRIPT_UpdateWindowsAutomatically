13b608b3ce73c0cde632bae3eff553dbbd2da18d
Mattia Caneva <canvacanva@gmail.com>
2023-08-23 16:24:43 +0200

    Update WU_AUTO-Reboot.ps1

--- WU_AUTO-Reboot.ps1
+++ WU_AUTO-Reboot.ps1
@@ -1,5 +1,5 @@
 ﻿Install-PackageProvider -Name NuGet -force 
 Install-Module -Name PSWindowsUpdate -force
-Install-WindowsUpdate -MicrosoftUpdate -AutoReboot -SendReport -PSWUSettings @{SmtpServer="o365relay.corp.amergroup.it";From="update_alert@amergroup.it";To="windows_update@amergroup.it";Port=25} -Verbose -Confirm:$False
+Install-WindowsUpdate -MicrosoftUpdate -AutoReboot -SendReport -PSWUSettings @{SmtpServer="relay";From="update_alert@domain";To="windows_update@domain";Port=25} -Verbose -Confirm:$False
 #Get-WUlist -MicrosoftUpdate
 #Get-WUHistory
