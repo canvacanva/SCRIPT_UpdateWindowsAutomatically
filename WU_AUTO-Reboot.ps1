0b194394422cf3c8c3d247926e21588bb99e7d8e
Mattia Caneva <canvacanva@gmail.com>
2023-08-23 16:24:03 +0200

    Add files via upload

---
+++ WU_AUTO-Reboot.ps1
@@ -0,0 +1,5 @@
+﻿Install-PackageProvider -Name NuGet -force 
+Install-Module -Name PSWindowsUpdate -force
+Install-WindowsUpdate -MicrosoftUpdate -AutoReboot -SendReport -PSWUSettings @{SmtpServer="o365relay.corp.amergroup.it";From="update_alert@amergroup.it";To="windows_update@amergroup.it";Port=25} -Verbose -Confirm:$False
+#Get-WUlist -MicrosoftUpdate
+#Get-WUHistory
---
+++ WU_NO-Reboot.ps1
@@ -0,0 +1,13 @@
+﻿Install-PackageProvider -Name NuGet -force
+Install-Module -Name PSWindowsUpdate -force
+Install-WindowsUpdate -MicrosoftUpdate -IgnoreReboot -SendReport -PSWUSettings @{SmtpServer="o365relay.corp.amergroup.it";From="update_alert@amergroup.it";To="windows_update@amergroup.it";Port=25} -Verbose -Confirm:$False
+
+$(Get-WURebootStatus -Silent -outvariable Status)
+
+if ($Status -like "True") {
+    Get-WURebootStatus -SendReport -PSWUSettings @{SmtpServer="o365relay.corp.amergroup.it";From="update_alert@amergroup.it";To="windows_update@amergroup.it";Port=25;} -Silent
+    } else
+    {
+    echo "No reboot needed"
+    }
+exit
