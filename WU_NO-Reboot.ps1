7b9419e2e7ac25fe015d31e9b784a604561ce34d
Mattia Caneva <canvacanva@gmail.com>
2023-08-24 17:32:37 +0200

    Update WU_NO-Reboot.ps1

--- WU_NO-Reboot.ps1
+++ WU_NO-Reboot.ps1
@@ -1,11 +1,11 @@
 ﻿Install-PackageProvider -Name NuGet -force
 Install-Module -Name PSWindowsUpdate -force
-Install-WindowsUpdate -MicrosoftUpdate -IgnoreReboot -SendReport -PSWUSettings @{SmtpServer="relay";From="update_alert@domain";To="windows_update@domain";Port=25} -Verbose -Confirm:$False
+Install-WindowsUpdate -MicrosoftUpdate -IgnoreReboot -SendReport -PSWUSettings @{SmtpServer="relay";From="alert@domain";To="update@domain";Port=25} -Verbose -Confirm:$False
 
 $(Get-WURebootStatus -Silent -outvariable Status)
 
 if ($Status -like "True") {
-    Get-WURebootStatus -SendReport -PSWUSettings @{SmtpServer="relay";From="update_alert@domain";To="windows_update@domain";Port=25;} -Silent
+    Get-WURebootStatus -SendReport -PSWUSettings @{SmtpServer="relay";From="alert@domain";To="pdate@domain";Port=25;} -Silent
     } else
     {
     echo "No reboot needed"
