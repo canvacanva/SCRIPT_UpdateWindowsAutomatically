c0196033219b8715792b5a0f9a1ff65d381db17d
Mattia Caneva <canvacanva@gmail.com>
2024-05-10 10:26:31 +0200

    Add files via upload
    Only Update alert

---
+++ Update_alerts.ps1
@@ -0,0 +1,3 @@
+﻿Install-PackageProvider -Name NuGet -force
+Install-Module -Name PSWindowsUpdate -force
+Get-WUList -Category 'Security Updates','Critical Updates' -SendReport -PSWUSettings @{SmtpServer="relay";From="security_update@sss";To="it@sss";Port=25;} -Silent
